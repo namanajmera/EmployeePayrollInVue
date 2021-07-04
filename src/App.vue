08bcc5a624bc7241746a14dd4438a62355f3c330
Naman <cool.naman.ajmera@gmail.com>
2021-07-04 21:01:47 +0530

    Added: Add the hello World and add the welcome message.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Hello World</h1>
+    <h2>Welcome to Employee Payroll App</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
